a040a003018880de5c1a749f4e0056841dc94fd4
Jake Klopfenstein <jake.klopfenstein@kellogg.northwestern.edu>
2021-01-31 19:03:58 -0600

    Start

--- noober.js
+++ noober.js
@@ -5,6 +5,25 @@ async function pageLoaded() {
   // writes the returned JSON to the console
   console.dir(json)
   
+
+  for (let i = 0; i < json.rides.length; i++) {
+    let rides = json.rides[i]
+    let outputElement = document.querySelector('.rides')
+    outputElement.insertAdjacentHTML('beforeend', `
+      <div class="p-4 w-full md:w-1/2 lg:w-1/3">
+        <div class="border h-full p-4 flex flex-col">
+          <h2 class="text-lg font-bold mb-4">${product.name}</h2>
+          <div class="mb-4"><img src=${product.image}>
+          </div>
+          <div class="mb-4 text-gray-900">${product.description}</div>
+          <div class="mt-auto text-purple-500 text-2xl">${product.price}</div>
+        </div>
+      </div>
+      
+      
+    `)
+  }
+
   // 🔥 start here: write code to loop through the rides
   
 }
